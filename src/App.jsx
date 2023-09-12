b2d3084a6c4a25a3425f4927d836a6b4f7ab4032
Nguyễn Hoàng Tuấn <nhtuan.job@gmail.com>
2023-09-12 19:12:34 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import GameDetail from "./pages/GameDetail";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
